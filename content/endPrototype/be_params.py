8e572e2c5007dcc15d9ac413e0a9a7fc5ea45c99
JobCollins <dulo.collins@gmail.com>
2024-03-01 12:22:35 +0300

    Chore:Clean Up

--- content/endPrototype/be_params.py
+++
@@ -1,5 +0,0 @@
-WANDB_PROJECT = "aerial-imagery-mlops"
-ENTITY = None # set this to team name if working in a team
-BE_CLASSES = {i:c for i,c in enumerate(['Water','Land (unpaved area)', 'Road','Building', 'Vegetation', 'Unlabeled'])}
-RAW_DATA_AT = 'big_earth_net'
-PROCESSED_DATA_AT = 'big_earth_net_split'
--- content/endPrototype/params.py
+++
@@ -1,5 +0,0 @@
-WANDB_PROJECT = "sem_seg"
-ENTITY = None # set this to team name if working in a team
-BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
-RAW_DATA_AT = 'archive'
-PROCESSED_DATA_AT = 'archive_split'
